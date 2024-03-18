5b807899261833ec37d50e30a077c05a94a7f48b
Philip Young <45668270+AusreisserSF@users.noreply.github.com>
2024-03-18 12:55:05 -0700

    The TextInput class is ready for integration into CenterStageBackdrop

--- src/sample/TextInput.java
+++ src/sample/TextInput.java
@@ -35,24 +35,15 @@ public class TextInput extends Application {
     @Override
     public void start(Stage primaryStage) {
         Scene scene = new Scene(new TestPane(), 300, 100);
-        /*
-        valueProperty.addListener(((observable, oldValue, newValue) -> {
-            System.out.println("Value changed -> Old Value: " + oldValue + ", New Value: " + newValue);
-
-            if (newValue < 0.0) {
-                Alert errorAlert = new Alert(Alert.AlertType.ERROR);
-                errorAlert.setHeaderText("Input not valid");
-                errorAlert.setContentText("The width of the robot must be between 8.0 and 18.0 inches");
-                errorAlert.showAndWait();
-            }
-        }));
-
-         */
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public class TestPane extends BorderPane {
+
+        private double widthParameter;
+        private boolean widthParameterValid = false;
+
         public TestPane() {
             TextField textField = new TextField();
 
@@ -61,89 +52,47 @@ public class TextInput extends Application {
             textField.setTextFormatter(textFormatter);
 
             valueProperty.addListener(new PredicateChangeListener(
-                    parameter -> parameter >= MIN_ROBOT_WIDTH && parameter <= MAX_ROBOT_WIDTH));
-
+                    widthP -> {
+                        widthParameter = widthP;
+                        widthParameterValid = widthP >= MIN_ROBOT_WIDTH && widthP <= MAX_ROBOT_WIDTH;
+                        return widthParameterValid;
+                    },
+                    "The width of the robot must be between 8.0 and 18.0 inches"));
 
             setCenter(new VBox(10,
                     new HBox(6, new Text("TextField 1"), textField),
                     new HBox(6, new Text("TextField 2"), new TextField())));
 
-            //double validatedParameter =
-            //validateRobotStartParameter(textField, (Double parameter) -> parameter >= MIN_ROBOT_WIDTH && parameter <= MAX_ROBOT_WIDTH);
             //System.out.println("Validated parameter: " + validatedParameter);
         }
 
-
-        // Here's another one from Fabian --
-// https://stackoverflow.com/questions/50102818/shared-changelistener-vs-multiple-changelisteners
-
+        // Based on this answer from Fabian --
+        // https://stackoverflow.com/questions/50102818/shared-changelistener-vs-multiple-changelisteners
         private class PredicateChangeListener implements ChangeListener<Double> {
             private final Predicate<Double> changePredicate;
+            private String errorMsg;
 
-            PredicateChangeListener(Predicate<Double> pChangePredicate) {
+            PredicateChangeListener(Predicate<Double> pChangePredicate, String pErrorMsg) {
                 changePredicate = pChangePredicate;
+                errorMsg = pErrorMsg;
             }
 
             @Override
             public void changed(ObservableValue<? extends Double> observable, Double oldValue, Double newValue) {
-
                 if (!changePredicate.test(newValue)) {
                     Alert errorAlert = new Alert(Alert.AlertType.ERROR);
                     errorAlert.setHeaderText("Input not valid");
-                    errorAlert.setContentText("The width of the robot must be between 8.0 and 18.0 inches");
+                    errorAlert.setContentText(errorMsg);
                     errorAlert.showAndWait();
                 } else
                     System.out.println("Value changed -> Old Value: " + oldValue + ", New Value: " + newValue);
-
             }
-
-        }
-
-
-        //**TODO This just doesn't work because it returns 0.0 on the first call.
-        private double validateRobotStartParameter(TextField pTextField, Predicate<Double> pTest) {
-            AtomicReference<Double> retVal = new AtomicReference<>((double) 0);
-            ObjectProperty<Double> valueProperty = new SimpleObjectProperty<>(0.0);
-            TextFormatter<Double> textFormatter = new TextFormatter<>(new DoubleStringConverter());
-            textFormatter.valueProperty().bindBidirectional(valueProperty);
-            pTextField.setTextFormatter(textFormatter);
-            valueProperty.addListener(((observable, oldValue, newValue) -> {
-                System.out.println("Value changed -> Old Value: " + oldValue + ", New Value: " + newValue);
-
-                if (!pTest.test(newValue)) {
-                    retVal.set(0.0);
-                    Alert errorAlert = new Alert(Alert.AlertType.ERROR);
-                    errorAlert.setHeaderText("Input not valid");
-                    errorAlert.setContentText("The width of the robot must be between 8.0 and 18.0 inches");
-                    errorAlert.showAndWait();
-                } else retVal.set(newValue);
-            }));
-
-            return retVal.get();
-        }
-    }
-
-    public static class PositiveDoubleStringConverter extends DoubleStringConverter {
-
-        @Override
-        public Double fromString(String value) {
-            double result = super.fromString(value);
-            if (result < 0) {
-                throw new RuntimeException("Negative number");
-            }
-            return result;
-        }
-
-        @Override
-        public String toString(Double value) {
-            if (value < 0) {
-                return "0";
-            }
-            return super.toString(value);
         }
 
     }
 
 }
+
+
 
 
