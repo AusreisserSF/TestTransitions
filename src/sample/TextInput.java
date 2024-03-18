496a164344384eb5c3fd622fee90afec6e1bb7eb
Philip Young <45668270+AusreisserSF@users.noreply.github.com>
2024-03-17 21:57:56 -0700

    WIP: startup parameters - experiment with validation of input

--- src/sample/TextInput.java
+++ src/sample/TextInput.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.BorderPane;
@@ -14,19 +15,27 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.converter.IntegerStringConverter;
-
-import java.util.function.UnaryOperator;
+import javafx.util.converter.DoubleStringConverter;
 
 public class TextInput extends Application {
+    private final ObjectProperty<Double> valueProperty = new SimpleObjectProperty<>(0.0);
 
-    private ObjectProperty<Integer> valueProperty = new SimpleObjectProperty<>(0);
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) {
         Scene scene = new Scene(new TestPane(), 300, 100);
         valueProperty.addListener(((observable, oldValue, newValue) -> {
             System.out.println("Value changed -> Old Value: " + oldValue + ", New Value: " + newValue);
+
+            if (newValue < 0.0) {
+                Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setHeaderText("Input not valid");
+                errorAlert.setContentText("The width of the robot must be between 8.0 and 18.0 inches");
+                errorAlert.showAndWait();
+            }
         }));
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -35,25 +44,36 @@ public class TextInput extends Application {
     public class TestPane extends BorderPane {
         public TestPane() {
             TextField textField = new TextField();
-            // new TextFormatter(new PositiveIntegerStringConverter(), 0, new PositiveIntegerFilter())
-            TextFormatter<Integer> textFormatter = new TextFormatter(new IntegerStringConverter()
-                    , 0, new IntegerFilter());
-           textFormatter.valueProperty().bindBidirectional(valueProperty);
+            TextFormatter<Double> textFormatter = new TextFormatter<>(new DoubleStringConverter());
+            textFormatter.valueProperty().bindBidirectional(valueProperty);
             textField.setTextFormatter(textFormatter);
-            setCenter(new HBox(6, new Text("TextField 1"), textField));
+            setCenter(new VBox(10,
+                    new HBox(6, new Text("TextField 1"), textField),
+                    new HBox(6, new Text("TextField 2"), new TextField())));
         }
     }
 
-    public class IntegerFilter implements UnaryOperator<TextFormatter.Change> {
+    public static class PositiveDoubleStringConverter extends DoubleStringConverter {
 
         @Override
-        public TextFormatter.Change apply(TextFormatter.Change change) {
-            if (change.getControlNewText().matches("-?([1-9][0-9]*)?")) {
-                return change;
+        public Double fromString(String value) {
+            double result = super.fromString(value);
+            if (result < 0) {
+                throw new RuntimeException("Negative number");
             }
-            return null;
+            return result;
         }
+
+        @Override
+        public String toString(Double value) {
+            if (value < 0) {
+                return "0";
+            }
+            return super.toString(value);
+        }
+
     }
 
 }
+
 
