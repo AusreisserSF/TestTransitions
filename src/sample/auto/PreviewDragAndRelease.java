46f387ab3dd9975a35f8d06d7e56db24ddcdcce5
Philip Young <45668270+AusreisserSF@users.noreply.github.com>
2024-05-08 20:12:55 -0700

    See //**TODO Boundary checking is off; false positive for out-of-bounds. In PreviewDragAndRelease.

--- src/sample/auto/PreviewDragAndRelease.java
+++ src/sample/auto/PreviewDragAndRelease.java
@@ -18,6 +18,8 @@ public class PreviewDragAndRelease {
     public static final String CAMERA_FOV_LINE_LEFT = "lineHalfFOVLeft";
     public static final String CAMERA_FOV_LINE_RIGHT = "lineHalfFOVRight";
 
+    private final Rectangle cameraOnRobot;
+    private final double aprilTagCenterY;
     private double orgSceneX, orgSceneY;
     private double orgRobotTranslateX, orgRobotTranslateY;
     private double previousRobotTranslateX, previousRobotTranslateY;
@@ -34,13 +36,13 @@ public class PreviewDragAndRelease {
 
     public PreviewDragAndRelease(CenterStageControllerLG pController, Pane pField,
                                  Rectangle pApproachZone, RobotFXCenterStageLG pPreviewRobot,
-                                 double pAprilTagX) {
+                                 double pTargetAprilTagX) {
 
         // Show the preview robot on the field.
         Group previewRobotGroup = pPreviewRobot.getRobot();
         pField.getChildren().add(previewRobotGroup);
 
-        Rectangle cameraOnRobot = (Rectangle) previewRobotGroup.lookup("#" + previewRobotGroup.getId() + "_" + RobotFXCenterStageLG.CAMERA_ON_ROBOT_ID);
+        cameraOnRobot = (Rectangle) previewRobotGroup.lookup("#" + previewRobotGroup.getId() + "_" + RobotFXCenterStageLG.CAMERA_ON_ROBOT_ID);
         Point2D cameraCoord = cameraOnRobot.localToScene(cameraOnRobot.getX(), cameraOnRobot.getY());
         double cameraFaceX = cameraCoord.getX() + cameraOnRobot.getWidth() / 2;
         double cameraFaceY = cameraCoord.getY();
@@ -48,7 +50,7 @@ public class PreviewDragAndRelease {
         // Get the y coordinates of any AprilTag - they're all the same.
         Rectangle aprilTag = (Rectangle) pField.lookup("#" + FieldFXCenterStageBackdropLG.APRIL_TAG_1_ID);
         Point2D aprilTagCoord = aprilTag.localToScene(aprilTag.getX(), aprilTag.getY());
-        double aprilTagCenterY = aprilTagCoord.getY() + aprilTag.getHeight() / 2;
+        aprilTagCenterY = aprilTagCoord.getY() + aprilTag.getHeight() / 2;
 
         // Get the adjacent side of the triangle from the camera to the AprilTag.
         double fovAdjacent = Math.abs(cameraFaceY - aprilTagCenterY);
@@ -123,17 +125,6 @@ public class PreviewDragAndRelease {
             pController.robot_position_at_backdrop_x.setText(String.format(Locale.US, "%.2f", previewRobotCenterInX));
             pController.robot_position_at_backdrop_y.setText(String.format(Locale.US, "%.2f", previewRobotCenterInY));
 
-            //**TODO It would be ideal to redraw the FOV lines as the preview
-            // robot is dragged withing the approach zone. Then it would be
-            // easy to know if the AprilTag is within the camera's FOV or not.
-            // Do this in two steps: 1. trigonometry, validation, and changing
-            // the font color of the FOV to red and 2. redraw the lines - try
-            // in the testbed first.
-            // From jewelsea's answer in
-            // https://stackoverflow.com/questions/24702542/how-to-change-the-color-of-text-in-javafx-textfield
-            // textField.setStyle("-fx-text-inner-color: red;");
-            //**TODO This works pController.camera_field_of_view.setStyle("-fx-text-inner-color: red; -fx-font-weight: bold;");
-
             // Drag the left boundary of the camera field of view.
             double newFOVLineLeftTranslateX = orgFOVLineLeftTranslateX + offsetX;
             double newFOVLineLeftTranslateY = orgFOVLineLeftTranslateY + offsetY;
@@ -145,6 +136,37 @@ public class PreviewDragAndRelease {
             double newFOVLineRightTranslateY = orgFOVLineRightTranslateY + offsetY;
             fovLineRight.setTranslateX(newFOVLineRightTranslateX);
             fovLineRight.setTranslateY(newFOVLineRightTranslateY);
+
+            //**TODO It would be ideal to redraw the FOV lines as the preview
+            // robot is dragged withing the approach zone. Then it would be
+            // easy to know if the AprilTag is within the camera's FOV or not.
+            // Do this in two steps: 1. trigonometry, validation, and changing
+            // the font color of the FOV to red and 2. redraw the lines - try
+            // in the testbed first.
+
+            //**TODO Boundary checking is off; false positive for out-of-bounds
+
+            Point2D updatedCameraCoord = cameraOnRobot.localToScene(cameraOnRobot.getX(), cameraOnRobot.getY());
+            double updatedCameraFaceX = updatedCameraCoord.getX() + cameraOnRobot.getWidth() / 2;
+            double updatedCameraFaceY = updatedCameraCoord.getY();
+
+            // Get the adjacent side of the triangle from the camera to the AprilTag.
+            double updatedFOVAdjacent = Math.abs(updatedCameraFaceY - aprilTagCenterY);
+            // tangent  = opposite / adjacent
+            double updatedHalfFOVTan = Math.tan(Math.toRadians(pPreviewRobot.cameraFieldOfView / 2));
+            double updatedHalfFOVOpposite = updatedHalfFOVTan * updatedFOVAdjacent;
+
+            double leftFOVBoundaryAtAprilTag = updatedCameraFaceX - updatedHalfFOVOpposite;
+            double rightFOVBoundaryAtAprilTag = updatedCameraFaceX + updatedHalfFOVOpposite;
+
+            if (pTargetAprilTagX < leftFOVBoundaryAtAprilTag || pTargetAprilTagX > rightFOVBoundaryAtAprilTag) {
+                // The target AprilTag is outside of the camera's field of view.
+                // From jewelsea's answer in
+                // https://stackoverflow.com/questions/24702542/how-to-change-the-color-of-text-in-javafx-textfield
+                // textField.setStyle("-fx-text-inner-color: red;");
+                pController.camera_field_of_view.setStyle("-fx-text-inner-color: red; -fx-font-weight: bold;");
+            } else
+                pController.camera_field_of_view.setStyle("-fx-text-inner-color: black; -fx-font-weight: normal;");
         });
     }
 }
